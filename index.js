c458e3d8136b0379c02051988ad8eaf08b1acd3b
omsaini-1441 <omsaini1441@gmail.com>
2025-06-26 23:23:09 +0530

    cors fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 require("./config/db");
 
 const app = express();
-app.use(cors({ origin: "http://localhost:5173" }));
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Routes
